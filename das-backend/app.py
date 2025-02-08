2d339405976cf16f092d8624523b7cc48147622f
derianlemus95 <derian.lemus95@gmail.com>
2025-02-07 18:35:38 -0800

    set boilerplate for frontend and backend

--- das-backend/app.py
+++ das-backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ das-frontend/src/Main/Main.js
@@ -0,0 +1,13 @@
+function Main() {
+    return(
+        <div>
+            <h2>1. DNA Sequence Analysis</h2>
+            <h4>Project Overview: Develop a web application that allows users to input DNA sequences and perform various analyses like: </h4>
+                <p>calculating GC content</p>
+                <p>finding motifs</p>
+                <p>comparing sequences using alignment algorithms</p>
+        </div>  
+)
+}
+
+export default Main
