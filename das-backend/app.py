7ddb15117a8ae2157b5a3cb1ef3582ed95aee414
derianlemus95 <derian.lemus95@gmail.com>
2025-03-07 17:12:58 -0800

    created apis for dna calculations

--- das-backend/app.py
+++ das-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -7,6 +7,42 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+#return status 200 if ok and returning data to client
+#return status 400 if error with data recived
+#return status 500 if error on server side
+@app.route('/calculation/dna-seq', methods=['POST'])
+def dna_seq():
+    dna_seq = request.json.get('dna_seq')
+    data = "e"
+    #if error with dna sequence return 400
+    if  (dna_seq.isalpha() == False) or len(dna_seq)==0:
+        return Response('Invalid DNA Sequence', status=400)
+    #if no error, calc gc content as percent
+    gc_calc = gc_content(dna_seq)
+    return jsonify(gc_calc), 200
+
+
+def gc_content(dna_seq):
+    #GC Content = 100% * COUNT(G+C) / COUNT(A+T+G+C)
+    gc_count = dna_seq.count("G") + dna_seq.count("C")
+    atgc_count = dna_seq.count("A") + dna_seq.count("T") + gc_count
+    gc_percent = 100 * gc_count / atgc_count
+    return gc_percent
+
+@app.route('/calculation/motif', methods=['POST'])
+def motif():
+    motif = request.json.get('motif')
+    dna_seq = request.json.get('dna_seq')
+    if (motif.isalpha() == False) or len(motif)==0 :
+        return Response('Invalid motif', status=400)
+
+    print(motif)
+    #return true if substring found else false
+    if motif not in dna_seq:
+        return Response("DNA sequence not found", status=404)
+    return Response("DNA sequence found", status=200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- das-frontend/src/Main/Main.js
+++ das-frontend/src/Main/Main.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react"
+
 function Main() {
+    //GC Content =100% * ( Count(G+C) / COUNT( A+T+G+C) )
+    const [dna_seq, setDnaSequence] = useState();
+
+    const handleSubmit = (event) => {
+        s
+    }
+    const handleDnaSequenceValueChange = (event) => {
+        setDnaSequence(event.target.value);     
+    }
+    useEffect(()=> {
+        const analizedSequence = async() => {
+            await axios.post("/calculation/dna-seq", {dna_seq: dna_seq})
+        }
+        analizedSequence();
+
+    }, []);
+
     return(
         <div>
             <h2>1. DNA Sequence Analysis</h2>
             <h4>Project Overview: Develop a web application that allows users to input DNA sequences and perform various analyses like: </h4>
-                <p>calculating GC content</p>
+                <form onSubmit={handleSubmit}>
+                    <p>Enter the DNA sequence to be analized or upload a .csv file</p>
+                    <text></text>
+                    <button>Submit</button>
+                </form>
+                <p>calculating GC content (determine the percentage of nitrogenous bases in a DNA or RNA molecule)</p>
                 <p>finding motifs</p>
                 <p>comparing sequences using alignment algorithms</p>
         </div>  
 )
 }
-
+//
 export default Main
